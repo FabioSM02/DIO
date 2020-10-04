cb85228089abb76e2a737de6bceb78b6a9c5ea12
Fabio Casa <fctrabnet@gmail.com>
2020-10-04 13:35:28 -0300

    Update script.js

--- Genesis/script.js
+++ Genesis/script.js
@@ -1,12 +1,3 @@
-/* 
-    SELECIONAR ALEATORIAMENTE AS CORES
-    ADICIONAR A CLASSE SELECTED-COLOR NA COR SELECIONADA
-    0 - green
-    1 - red
-    2 - yellow
-    3 - blue
-*/
-
 let order = [];
 let clickedOrder = [];
 let score = 0;
@@ -16,7 +7,6 @@ const red = document.querySelector('.red');
 const yellow = document.querySelector('.yellow');
 const blue = document.querySelector('blue');
 
-// Cria ordem aleatoria de cores
 let shuffleOrder = () => {
     let colorOrder = Math.floor(Math.random() * 4);
     order[order.length] = colorOrder;
@@ -29,7 +19,6 @@ let shuffleOrder = () => {
     }
 }
 
-// Acende a proxima cor
 let lightColor = (element, number) => {
     number = number * 500;
     setTimeout(() => {
@@ -40,7 +29,6 @@ let lightColor = (element, number) => {
     });
 }
 
-// Checa se os botões clicados são os mesmos da ordem gerada no jogo
 let checkOrder = () => {
     for (let i in clickedOrder) {
         if (clickedOrder[i] != order[i]) {
@@ -54,7 +42,6 @@ let checkOrder = () => {
     }
 }
 
-// função para o clique do usuario
 let click = (color) => {
     clickedOrder[clickedOrder.length] = color;
     createColorElement(color).classList.add('selected-color');
@@ -65,7 +52,6 @@ let click = (color) => {
     }, 250)
 }
 
-// criar a função que retorna a cor
 let createColorElement = (color) => {
     if (color == 0) {
         return green;
@@ -78,13 +64,11 @@ let createColorElement = (color) => {
     }
 }
 
-// função para o proximo nivel do jogo
 let nextLevel = () => {
     score++;
     shuffleOrder();
 }
 
-// função para gameOver
 let gameOver = () => {
     alert(`Pontuação: ${score}! \nVocê perdeu o jogo!\n Clique em ok para iniciar um novo jogo`);
     order = [];
@@ -92,7 +76,6 @@ let gameOver = () => {
     playGame();
 }
 
-// Função de inicio do jogo
 let playGame = () => {
     alert('Bem vindo ao Gênesis! Iniciando novo jogo!');
     score = 0;
@@ -100,16 +83,9 @@ let playGame = () => {
     nextLevel();
 }
 
-// green.addEventListener('click', click(0));
-// red.addEventListener('click', click(1));
-// yellow.addEventListener('click', click(2));
-// blue.addEventListener('click', click(3));
-
-// eventos de clique para as cores
 green.onclick = () => click(0);
 red.onclick = () => click(1);
 yellow.onclick = () => click(2);
 blue.onclick = () => click(3);
 
-// inicio do jogo
 playGame();
