6061146bfa6174c1d9c35601a3c3ef23fc1f9faa
Fabio Casa <fctrabnet@gmail.com>
2020-10-28 16:33:30 -0300

    Update script.js

--- Dino Game/script.js
+++ Dino Game/script.js
@@ -18,20 +18,18 @@ function jump() {
 
   let upInterval = setInterval(() => {
     if (position >= 150) {
-      // Descendo
       clearInterval(upInterval);
 
       let downInterval = setInterval(() => {
         if (position <= 0) {
-          clearInterval(downInterval);
-          isJumping = false;
+            clearInterval(downInterval);
+            isJumping = false;
         } else {
-          position -= 20;
-          dino.style.bottom = position + 'px';
+            position -= 20;
+            dino.style.bottom = position + 'px';
         }
       }, 20);
     } else {
-      // Subindo
       position += 20;
       dino.style.bottom = position + 'px';
     }
@@ -45,25 +43,23 @@ function createCactus() {
 
   if (isGameOver) return;
 
-  cactus.classList.add('cactus');
-  background.appendChild(cactus);
-  cactus.style.left = cactusPosition + 'px';
+    cactus.classList.add('cactus');
+    background.appendChild(cactus);
+    cactus.style.left = cactusPosition + 'px';
 
-  let leftTimer = setInterval(() => {
-    if (cactusPosition < -60) {
-      // Saiu da tela
-      clearInterval(leftTimer);
-      background.removeChild(cactus);
-    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
-      // Game over
-      clearInterval(leftTimer);
-      isGameOver = true;
-      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
-    } else {
-      cactusPosition -= 10;
-      cactus.style.left = cactusPosition + 'px';
-    }
-  }, 20);
+    let leftTimer = setInterval(() => {
+      if (cactusPosition < -60) {
+          clearInterval(leftTimer);
+          background.removeChild(cactus);
+      } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+          clearInterval(leftTimer);
+          isGameOver = true;
+          document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+      } else {
+        cactusPosition -= 10;
+        cactus.style.left = cactusPosition + 'px';
+      }
+    }, 20);
 
   setTimeout(createCactus, randomTime);
 }
